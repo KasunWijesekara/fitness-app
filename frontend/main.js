f537627b55ebab9e989ef90ed060b4bc8e885893
Kasun Wijesekara <kasunwijesekara@ymail.com>
2023-11-20 03:14:30 +0530

    Update chat container and message classes

--- frontend/main.js
+++ frontend/main.js
@@ -1,30 +1,29 @@
 // No need to wait for the entire DOM content to load
-document.getElementById('chat-container').classList.add('chat-closed');
+document.getElementById('mcp_chat-container').classList.add('mcp_chat-closed');
 
 function updateScroll() {
-  var element = document.getElementById("chat-messages");
+  var element = document.getElementById("mcp_chat-messages");
   element.scrollTop = element.scrollHeight; // Scrolls to the bottom
 }
 
-document.getElementById('chat-send-button').addEventListener('click', function() {
-  var message = document.getElementById('chat-input').value;
+document.getElementById('mcp_chat-send-button').addEventListener('click', function() {
+  var message = document.getElementById('mcp_chat-input').value;
   if (message) {
     sendMessage(message);
-    document.getElementById('chat-input').value = ''; // Clear the input after sending
+    document.getElementById('mcp_chat-input').value = ''; // Clear the input after sending
   }
 });
 
 function sendMessage(message) {
   // Display the user's message
-  var messagesContainer = document.getElementById('chat-messages');
+  var messagesContainer = document.getElementById('mcp_chat-messages');
   var userMessageDiv = document.createElement('div');
   userMessageDiv.textContent = message;
-  userMessageDiv.className = 'user-message';
+  userMessageDiv.className = 'mcp_user-message'; // Update class if needed
   messagesContainer.appendChild(userMessageDiv);
 
   // Display typing indicator
   showTypingIndicator();
-
 
   // Send the message to the Flask backend
   fetch('http://127.0.0.1:5000/chatbot/message', {
@@ -36,7 +35,6 @@ function sendMessage(message) {
   })
   .then(response => response.json())
   .then(data => {
-    // console.log(data);  // add this line
     setTimeout(() => {
       displayBotMessage(data.response);
     }, 1000);
@@ -47,21 +45,21 @@ function sendMessage(message) {
 }
 
 function showTypingIndicator() {
-  document.getElementById('typing-indicator').style.display = 'flex';
+  document.getElementById('mcp_typing-indicator').style.display = 'flex';
   updateScroll(); // Now this function exists and will be called correctly
 }
 
 function hideTypingIndicator() {
-  document.getElementById('typing-indicator').style.display = 'none';
+  document.getElementById('mcp_typing-indicator').style.display = 'none';
 }
 
 function displayBotMessage(message) {
   // Hide typing indicator
   hideTypingIndicator();
-  var messagesContainer = document.getElementById('chat-messages');
+  var messagesContainer = document.getElementById('mcp_chat-messages');
   var botMessageDiv = document.createElement('div');
   botMessageDiv.textContent = message;
-  botMessageDiv.className = 'bot-message';
+  botMessageDiv.className = 'mcp_bot-message'; // Update class if needed
   messagesContainer.appendChild(botMessageDiv);
 
   // Scroll to the bottom of the messages container
@@ -72,56 +70,56 @@ function displayBotMessage(message) {
 }
 
 function addMessageToChat(message, isUserMessage) {
-  var messagesContainer = document.getElementById('chat-messages');
+  var messagesContainer = document.getElementById('mcp_chat-messages');
   var messageDiv = document.createElement('div');
   messageDiv.textContent = message;
-  messageDiv.className = isUserMessage ? 'user-message' : 'bot-message';
+  messageDiv.className = isUserMessage ? 'mcp_user-message' : 'mcp_bot-message'; // Update class if needed
   messagesContainer.appendChild(messageDiv);
 
   // Wait for the message to be rendered before scrolling
   setTimeout(updateScroll, 0);
 }
 
-document.getElementById('chat-input').addEventListener('keypress', function(e) {
+document.getElementById('mcp_chat-input').addEventListener('keypress', function(e) {
   if (e.key === 'Enter') {
-      document.getElementById('chat-send-button').click();
+    document.getElementById('mcp_chat-send-button').click();
   }
 });
 
 // Ensure the chat box is minimized on page load
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('chat-container').classList.add('chat-closed');
+  document.getElementById('mcp_chat-container').classList.add('mcp_chat-closed');
 });
 
 // JavaScript to toggle chat box visibility
-document.getElementById('chat-toggle').addEventListener('click', function() {
-  var chatContainer = document.getElementById('chat-container');
-  var chatToggleButton = document.getElementById('chat-toggle');
-  chatContainer.classList.toggle('chat-closed');
-  chatToggleButton.classList.toggle('chat-open'); // Add class to hide the toggle button when chat is open
+document.getElementById('mcp_chat-toggle').addEventListener('click', function() {
+  var chatContainer = document.getElementById('mcp_chat-container');
+  var chatToggleButton = document.getElementById('mcp_chat-toggle');
+  chatContainer.classList.toggle('mcp_chat-closed');
+  chatToggleButton.classList.toggle('chat-open'); // Update class if needed
 });
 
-document.getElementById('chat-close-button').addEventListener('click', function() {
-  var chatContainer = document.getElementById('chat-container');
-  var chatToggleButton = document.getElementById('chat-toggle');
+document.getElementById('mcp_chat-close-button').addEventListener('click', function() {
+  var chatContainer = document.getElementById('mcp_chat-container');
+  var chatToggleButton = document.getElementById('mcp_chat-toggle');
 
-  // Add the 'chat-closed' class to hide the chat container
-  chatContainer.classList.add('chat-closed');
+  // Add the 'mcp_chat-closed' class to hide the chat container
+  chatContainer.classList.add('mcp_chat-closed');
 
   // Remove the 'chat-open' class to show the toggle button
-  chatToggleButton.classList.remove('chat-open');
+  chatToggleButton.classList.remove('chat-open'); // Update class if needed
 });
 
 let welcomeMessageShown = false;
 
-document.getElementById('chat-toggle').addEventListener('click', function() {
+document.getElementById('mcp_chat-toggle').addEventListener('click', function() {
   if (!welcomeMessageShown) {
-    displayBotMessage('Hello wellcome to fitness world, how can i help you?');
+    displayBotMessage('Hello welcome to fitness world, how can I help you?');
     welcomeMessageShown = true;
   }
 
   // Add a delay before showing the messages
   setTimeout(function() {
-    document.getElementById('chat-messages').style.opacity = 1;
+    document.getElementById('mcp_chat-messages').style.opacity = 1;
   }, 200);
 });
