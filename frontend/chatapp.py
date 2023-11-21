937abe52943d067450d87a755ab6056e935b7a6d
Kasun Wijesekara <kasunwijesekara@ymail.com>
2023-11-22 03:39:40 +0530

    Refactor chat_with_bot function to use async/await syntax

--- frontend/chatapp.py
+++ frontend/chatapp.py
@@ -1,19 +1,15 @@
 import os
 from dotenv import load_dotenv
-
 import logging
 import time
-
-
+import asyncio
 from flask import Flask, request, jsonify, make_response
 from flask.blueprints import Blueprint
 from flask_cors import CORS
-import openai
+from openai import OpenAI, APIConnectionError, RateLimitError, APIStatusError
 
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
-
-AUTHORIZED_TOKENS = {"abc123": "website1.com", "xyz789": "website2.com"}
 
 app = Flask(__name__)
 CORS(
@@ -24,6 +20,8 @@ CORS(
 )
 chatbot_blueprint = Blueprint("chatbot", __name__)
 
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 @app.route("/")
 def index():
@@ -31,27 +29,38 @@ def index():
 
 
 @chatbot_blueprint.route("/message", methods=["POST"])
-def chat_with_bot():
+async def chat_with_bot():
     start_time = time.time()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
     user_message = request.json.get("message", "")
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a helpful assitant that helps the user.",
-            },
-            {"role": "user", "content": user_message},
-        ],
-        max_tokens=500,
-        temperature=0.1,
-    )
+
+    try:
+        response = await client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assitant that helps the user.",
+                },
+                {"role": "user", "content": user_message},
+            ],
+            max_tokens=500,
+            temperature=0.1,
+        )
+        response_message = response.choices[0].message["content"].strip()
+    except APIConnectionError as e:
+        app.logger.error("Server could not be reached: " + str(e))
+        return make_response(jsonify({"error": "Server could not be reached"}), 503)
+    except RateLimitError as e:
+        app.logger.error("Rate limit error: " + str(e))
+        return make_response(jsonify({"error": "Rate limit exceeded"}), 429)
+    except APIStatusError as e:
+        app.logger.error("API error: " + str(e))
+        return make_response(jsonify({"error": "API error occurred"}), e.status_code)
+
     end_time = time.time()
     app.logger.info(f"Time taken: {end_time - start_time} seconds")
-    response_message = response.choices[0].message["content"].strip()
-    response = make_response(jsonify({"response": response_message}))
-    return response
+
+    return make_response(jsonify({"response": response_message}))
 
 
 # Register the blueprint after all routes have been added
@@ -59,4 +68,4 @@ app.register_blueprint(chatbot_blueprint, url_prefix="/frontend/chatbot")
 CORS(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    asyncio.run(app.run(debug=True))
