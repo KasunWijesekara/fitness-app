7b8d0aa4753d805d7a0755495a276351d13824f7
Kasun Wijesekara <kasunwijesekara@ymail.com>
2023-11-30 15:35:49 +0530

    Update database URI in frontend/database.py

--- frontend/database.py
+++ frontend/database.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://kasun:123kasun@localhost/fitnessdb"
+] = "postgresql://kasun:123kasun@fitness-app_db_1/fitnessdb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
