698e0a8ec3dc696bc6d18366f0b7a38101dfd1cb
Kasun Wijesekara <kasunwijesekara@ymail.com>
2023-12-01 01:48:44 +0530

    Fix session ID generation and IP address retrieval

--- frontend/run.py
+++ frontend/run.py
@@ -74,11 +74,9 @@ def chat_with_bot():
     session_id = request.cookies.get("session_id")
     if session_id is None:
         session_id = str(uuid.uuid4())
-    app.logger.debug(f"New session ID generated: {session_id}")
+        app.logger.debug(f"New session ID generated: {session_id}")
 
-    ip_address = request.headers.get(
-        "X-Forwarded-For", request.remote_addr
-    )  # Get the client's IP address
+    ip_address = request.remote_addr
 
     # Save the chat to the database
     new_chat = Chat(
