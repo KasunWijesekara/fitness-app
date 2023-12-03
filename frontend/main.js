a2dc855ad9fbd38d023f62b379dc7a5009076a89
Kasun Wijesekara <kasunwijesekara@ymail.com>
2023-12-04 02:43:46 +0530

    Add character counter and limit message length

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,25 @@
 // No need to wait for the entire DOM content to load
 document.getElementById('mcp_chat-container').classList.add('mcp_chat-closed');
 
+// Set the maximum number of characters allowed
+const maxChars = 200;
+
+document.getElementById('mcp_chat-input').addEventListener('input', function() {
+  var inputField = document.getElementById('mcp_chat-input');
+  var charCounter = document.getElementById('mcp_char-counter');
+  var message = inputField.value;
+
+  // Update the character counter
+  if (charCounter) {
+    charCounter.textContent = message.length + '/' + maxChars;
+  }
+
+  // If message exceeds maxChars, truncate it
+  if (message.length > maxChars) {
+    inputField.value = message.substr(0, maxChars);
+  }
+});
+
 function updateScroll() {
   var element = document.getElementById("mcp_chat-messages");
   element.scrollTop = element.scrollHeight; // Scrolls to the bottom
@@ -14,7 +33,21 @@ document.getElementById('mcp_chat-send-button').addEventListener('click', functi
   }
 });
 
+function generateNewSessionId() {
+  return uuid.v4();
+}
+
 function sendMessage(message) {
+  // Limit the message to maxChars just in case
+  message = message.substr(0, maxChars);
+
+  // Disable the input box and change its container's color to gray
+  var inputField = document.getElementById('mcp_chat-input');
+  var inputContainer = document.getElementById('mcp_chat-input-container');
+  inputField.disabled = true;
+  inputContainer.style.backgroundColor = '#e1e1e1';
+  inputField.style.backgroundColor = '#e1e1e1';
+
   // Display the user's message
   var messagesContainer = document.getElementById('mcp_chat-messages');
   var userMessageDiv = document.createElement('div');
@@ -22,16 +55,25 @@ function sendMessage(message) {
   userMessageDiv.className = 'mcp_user-message'; // Update class if needed
   messagesContainer.appendChild(userMessageDiv);
 
+  // Get the session ID from local storage
+  var sessionId = localStorage.getItem('sessionId');
+  if (!sessionId) {
+    // Generate a new session ID if one doesn't exist
+    sessionId = generateNewSessionId();
+    localStorage.setItem('sessionId', sessionId);
+  }
+
   // Display typing indicator
   showTypingIndicator();
 
   // Send the message to the Flask backend
-  fetch('https://0d16-112-134-219-253.ngrok-free.app/frontend/chatbot/message', {
+  fetch('https://web.01api.online/frontend/chatbot/message', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ message: message }),
+    body: JSON.stringify({ message: message, session_id: sessionId }),
+    credentials: 'include', // Include credentials in the request
   })
   .then(response => response.json())
   .then(data => {
@@ -57,14 +99,21 @@ function displayBotMessage(message) {
   // Hide typing indicator
   hideTypingIndicator();
 
-var messagesContainer = document.getElementById('mcp_chat-messages');
-var botMessageDiv = document.createElement('div');
+  // Enable the input box and change its container's color back to white
+  var inputField = document.getElementById('mcp_chat-input');
+  var inputContainer = document.getElementById('mcp_chat-input-container');
+  inputField.disabled = false;
+  inputField.style.backgroundColor = '#ffffff';
+  inputContainer.style.backgroundColor = '#ffffff';
 
-message = message.replace(/\n/g, "<br/>"); // Replace newline characters with <br/> tags
-botMessageDiv.innerHTML = message; // Use innerHTML instead of textContent to render the HTML tags
+  var messagesContainer = document.getElementById('mcp_chat-messages');
+  var botMessageDiv = document.createElement('div');
 
-botMessageDiv.className = 'mcp_bot-message'; // Update class if needed
-messagesContainer.appendChild(botMessageDiv);
+  message = message.replace(/\n/g, "<br/>"); // Replace newline characters with <br/> tags
+  botMessageDiv.innerHTML = message; // Use innerHTML instead of textContent to render the HTML tags
+
+  botMessageDiv.className = 'mcp_bot-message'; // Update class if needed
+  messagesContainer.appendChild(botMessageDiv);
 
   // Scroll to the bottom of the messages container
   messagesContainer.scrollTop = messagesContainer.scrollHeight;
@@ -73,26 +122,22 @@ messagesContainer.appendChild(botMessageDiv);
   updateScroll();
 }
 
-function addMessageToChat(message, isUserMessage) {
-  var messagesContainer = document.getElementById('mcp_chat-messages');
-  var messageDiv = document.createElement('div');
-  messageDiv.textContent = message;
-  messageDiv.className = isUserMessage ? 'mcp_user-message' : 'mcp_bot-message'; // Update class if needed
-  messagesContainer.appendChild(messageDiv);
-
-  // Wait for the message to be rendered before scrolling
-  setTimeout(updateScroll, 0);
-}
-
 document.getElementById('mcp_chat-input').addEventListener('keypress', function(e) {
   if (e.key === 'Enter') {
     document.getElementById('mcp_chat-send-button').click();
   }
 });
 
-// Ensure the chat box is minimized on page load
+// Continue from the last function
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('mcp_chat-container').classList.add('mcp_chat-closed');
+
+  // Create the character counter element
+  var inputField = document.getElementById('mcp_chat-input');
+  var charCounterDiv = document.createElement('div');
+  charCounterDiv.id = 'mcp_char-counter';
+  charCounterDiv.textContent = '0/' + maxChars;
+  inputField.parentNode.insertBefore(charCounterDiv, inputField.nextSibling); // Insert the counter after the input field
 });
 
 // JavaScript to toggle chat box visibility
