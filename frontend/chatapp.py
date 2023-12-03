95dff8f2c4dbdbc64d88bc03f5cd850974dede8e
Kasun Wijesekara <kasunwijesekara@ymail.com>
2023-12-04 02:57:16 +0530

    Update IP address retrieval method in chatapp.py

--- frontend/chatapp.py
+++ frontend/chatapp.py
@@ -85,7 +85,7 @@ def chat_with_bot():
         session_id = str(uuid.uuid4())
         app.logger.debug(f"New session ID generated: {session_id}")
 
-    ip_address = request.remote_addr
+    ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
 
     # Save the chat to the database
     new_chat = Chat(
